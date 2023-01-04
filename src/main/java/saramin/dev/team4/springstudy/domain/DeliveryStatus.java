caf71825e2df5e984e31f05139aea1b77af80829
EuiSung <gowoonsori97@naver.com>
2023-01-04 10:37:14 +0900

    Init 파일 push

--- src/main/java/saramin/dev/team4/springstudy/domain/DeliveryStatus.java
+++
@@ -1,5 +0,0 @@
-package saramin.dev.team4.springstudy.domain;
-
-public enum DeliveryStatus {
-    READY, COMP
-}
--- src/main/java/saramin/dev/team4/springstudy/domain/OrderStatus.java
+++
@@ -1,5 +0,0 @@
-package saramin.dev.team4.springstudy.domain;
-
-public enum OrderStatus {
-    ORDER, CANCEL
-}
